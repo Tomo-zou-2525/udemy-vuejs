bbc06c2486cb28371e62f328b3de1ed044d29904
友造-2525 <bathbook1990tomozou@gmail.com>
2020-08-13 09:54:34 +0900

    CSS導入まで

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <p>いいね{{num}}</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      num: 5
+    };
+  },
+  methods: {
+    increment() {
+      this.num += 1;
+    }
+  }
+};
+</script>
+
+<style>
+div {
+  border: 1px solid red;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from "vue";
 import App from "./App.vue";
+import Like from "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false;
+Vue.component("LikeN", Like);
 
 new Vue({
   render: h => h(App)
